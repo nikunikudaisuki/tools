f6d0fa9f8440990db6552d495684127fd024dd15
chie8842 <chie-hayashida@cookpad.com>
2019-08-20 22:30:26 +0800

    add deploy scripts

---
+++ deploy/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+source setenv.sh
+
+if [ -e result.txt ]; then
+  rm result.txt
+
+./notify.sh $USERNAME "start deployment at ${IPADDR1}"
+ssh isucon@$IPADDR1 'sh' < deploy_body.sh >> result.txt
+./notify.sh $USERNAME "start deployment at ${IPADDR2}"
+ssh isucon@$IPADDR2 'sh' < deploy_body.sh >> result.txt
+./notify.sh $USERNAME "start deployment at ${IPADDR3}"
+ssh isucon@$IPADDR3 'sh' < deploy_body.sh >> result.txt
+./notify.sh $USERNAME "finished deployment"
+result=`cat result.txt`
+./notify.sh $USERNAME result
