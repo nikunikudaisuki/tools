ebd4ba7a417f2f41da26d6b70682c516ef969d00
chie8842 <chie-hayashida@cookpad.com>
2019-08-23 19:08:34 +0800

    fixed notify.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,12 +6,15 @@ source ../setenv.sh
 if [ -e result.txt ]; then
   rm result.txt
 
-../notify.sh $USERNAME "start deployment at ${IPADDR1}"
+CHANNEL=test
+USERNAME=chie8842
+
+../notify.sh $CHANNEL $WEBHOOKURL $USERNAME "start deployment at ${IPADDR1}"
 ssh isucon@$IPADDR1 'sh' < deploy_body.sh >> result.txt
-../notify.sh $USERNAME "start deployment at ${IPADDR2}"
+../notify.sh $CHANNEL $WEBHOOKURL $USERNAME "start deployment at ${IPADDR2}"
 ssh isucon@$IPADDR2 'sh' < deploy_body.sh >> result.txt
-../notify.sh $USERNAME "start deployment at ${IPADDR3}"
+../notify.sh $CHANNEL $WEBHOOKURL $USERNAME "start deployment at ${IPADDR3}"
 ssh isucon@$IPADDR3 'sh' < deploy_body.sh >> result.txt
-../notify.sh $USERNAME "finished deployment"
+../notify.sh $CHANNEL $WEBHOOKURL $USERNAME "finished deployment"
 result=`cat result.txt`
-../notify.sh $USERNAME result
+../notify.sh $CHANNEL $WEBHOOKURL $USERNAME result
--- notify.sh
+++ notify.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+source setenv.sh
+echo $WEBHOOKURL
 DATA_PAYLOAD=`cat <<_EOT_
 payload={ \
-      "channel": "#isucon8", \
-          "username": "$1", \
-              "text": "$2" \
-            }
+      "channel": "#test", \
+      "username": "${1}", \
+      "text": "@${3} ${4}" \
+      }
 _EOT_`
-
-curl -s -S -X POST --data-urlencode "${DATA_PAYLOAD}" "${WEBHOOKURL}" > /dev/null
+echo $DATA_PAYLOAD
+curl -s -S -X POST --data-urlencode "${DATA_PAYLOAD}" "$2" > /dev/null
