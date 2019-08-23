798e54309def1d60a9a19be31c17616826f1cd09
chie8842 <chie-hayashida@cookpad.com>
2019-08-23 15:37:46 +0800

    update bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,10 +3,11 @@ source ../setenv.sh
 
 if [ -e result.txt ]; then
   rm result.txt
+fi
 
-../utils/notify.sh $USERNAME "start bootstrap at ${IPADDR1}"
-ssh isucon@$IPADDR1 'sh' < bootstrap_body.sh isucon81 >> result.txt
-../utils/notify.sh $USERNAME "start bootstrap at ${IPADDR2}"
-ssh isucon@$IPADDR2 'sh' < bootstrap_body.sh isucon82 >> result.txt
-../utils/notify.sh $USERNAME "start bootstrap at ${IPADDR3}"
-ssh isucon@$IPADDR3 'sh' < bootstrap_body.sh isucon83 >> result.txt
+../notify.sh $USERNAME "start bootstrap at ${IPADDR1}"
+ssh isucon@$IPADDR1 'sh -s' < bootstrap_body.sh isucon81 >> result.txt
+../notify.sh $USERNAME "start bootstrap at ${IPADDR2}"
+ssh isucon@$IPADDR2 'sh -s' < bootstrap_body.sh isucon82 >> result.txt
+../notify.sh $USERNAME "start bootstrap at ${IPADDR3}"
+ssh isucon@$IPADDR3 'sh -s' < bootstrap_body.sh isucon83 >> result.txt
